ce7073c4e4c4fb74a76fd18db0bc0b06c10d41dd
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-11-11 22:36:46 +0530

    styled buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
+import ReactLoading from "react-loading";
 
-import './App.css'
-
+import "./App.css";
 
 function App() {
   const [dayCount, setDayCount] = useState([]);
   const [destination, setDestination] = useState("");
   const [budget, setBudget] = useState("");
   const [travelPlan, setTravelPlan] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -15,6 +16,7 @@ function App() {
     //validate the form
     if (!destination || !budget || !dayCount) {
       alert("Please enter all the fields ");
+      setLoading(false);
       return;
     }
 
@@ -29,8 +31,7 @@ function App() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization:
-          "Bearer " + import.meta.env.VITE_OPENAI_API,
+        Authorization: "Bearer " + import.meta.env.VITE_OPENAI_API,
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo-instruct",
@@ -40,79 +41,97 @@ function App() {
       }),
     };
     try {
-        const response = await fetch("https://api.openai.com/v1/completions",requestOptions);
-        const data = await response.json()
+      const response = await fetch(
+        "https://api.openai.com/v1/completions",
+        requestOptions
+      );
+      const data = await response.json();
 
-        setTravelPlan(data.choices[0].text)
+      setTravelPlan(data.choices[0].text);
 
-        var travel_plan = data.choices[0].text;
-        var travel_plan_array = travel_plan.split("\n")
+      var travel_plan = data.choices[0].text;
+      var travel_plan_array = travel_plan.split("\n");
 
-        setTravelPlan(travel_plan_array)
+      setTravelPlan(travel_plan_array);
     } catch (error) {
-        console.log(error)
-        alert(error)
+      console.log(error);
+      alert(error);
+      setLoading(false)
     }
-    
   };
   return (
     <>
-        <h1 className="font-Rampart text-3xl">Plan My Trip</h1>
-        {!travelPlan && <form onSubmit={handleFormSubmit}
-        className=' m-10'
-        >
-          
-          
-            <input
-              type="text"
-              value={destination}
-              onChange={(e) => setDestination(e.target.value)}
-              className="shadow appearance-none border rounded m-3 p-3 text-black leading-tight focus:outline-none focus:shadow-outline"
-              placeholder='Enter your destination...'
-            />
-    
+      <h1 className="font-Rampart text-3xl">Plan My Trip </h1>
+      {!travelPlan && (
+        <form onSubmit={handleFormSubmit} className=" m-10">
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            className="shadow appearance-none border rounded m-3 p-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Enter your destination..."
+          />
+
           <br />
-          
-            <input
-              type="text"
-              value={budget}
-              onChange={(e) => setBudget(e.target.value)}
-              className="block shadow appearance-none border rounded m-3 p-3 text-black leading-tight focus:outline-none focus:shadow-outline"
-              placeholder='Enter your budget'
-            />
-            <input
-              type="text"
-              value={dayCount}
-              onChange={(e) => setDayCount(e.target.value)}
-              className="shadow appearance-none border rounded m-3 p-3 text-black leading-tight focus:outline-none focus:shadow-outline"
-              placeholder='Enter number of days'
-            />
-          
+
+          <input
+            type="text"
+            value={budget}
+            onChange={(e) => setBudget(e.target.value)}
+            className="block shadow appearance-none border rounded m-3 p-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Enter your budget"
+          />
+          <input
+            type="text"
+            value={dayCount}
+            onChange={(e) => setDayCount(e.target.value)}
+            className="shadow appearance-none border rounded m-3 p-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Enter number of days"
+          />
+
           <br />
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded focus:outline-none focus:shadow-outline"
-          >
-            Generate Travel Plan
-          </button>
-      
+          {!loading ? (
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded focus:outline-none focus:shadow-outline"
+              onClick={() => setLoading(true)}
+            >
+              Generate Travel Plan
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded focus:outline-none focus:shadow-outline"
+            >
+           
+              <ReactLoading type="spinningBubbles" color="#fffff"
+                height={25} width={30}  />
+            </button>
+          )}
         </form>
-}
-        {travelPlan && (
-          <div className="bg-gray-500 rounded-lg shadow-lg p-2 m-2">
-            <h2 className="block font-bold mb-2 text-white">
-              Generated Travel Plan:
-            </h2>
-            <div >
-              {travelPlan.map((item, index) => (
-                item.includes("Day")|| item.includes("Budget breakdown") ? <h3 className="block font-bold mb-2 text-black" key={index}>{item}</h3> : <p className="block font-bold mb-2 text-white" key={index}>{item}</p>
-              ))}
-            </div>
+      )}
+      {travelPlan && (
+        <div className="bg-gray-500 rounded-lg shadow-lg p-2 m-2">
+          <h2 className="block font-bold mb-2 text-white">
+            Generated Travel Plan:
+          </h2>
+          <div>
+            {travelPlan.map((item, index) =>
+              item.includes("Day") || item.includes("Budget breakdown") ? (
+                <h3 className="block font-bold mb-2 text-black" key={index}>
+                  {item}
+                </h3>
+              ) : (
+                <p className="block font-bold mb-2 text-white" key={index}>
+                  {item}
+                </p>
+              )
+            )}
           </div>
-        )}
+        </div>
+      )}
     </>
-  )
-
+  );
 }
 
-export default App
+export default App;
